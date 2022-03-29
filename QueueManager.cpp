13b0a5a62baa953d1c49437d9bddcaab52a7be7b
tooo <toooo123@live.ca>
2022-03-29 14:17:49 -0700

    Clean up QueueManager

--- QueueManager.cpp
+++ QueueManager.cpp
@@ -4,56 +4,54 @@
 
 void QueueManager::enqueuePQueue(Patient patient) {
 
-    switch(patient.priority){
+    switch (patient.priority) {
 
         case Priority::HIGH:
             high_queue.push(patient);
-        
+            break;
         case Priority::MEDIUM:
             medium_queue.push(patient);
-
+            break;
         case Priority::LOW:
             low_queue.push(patient);
-
+            break;
     }
 }
 
 Patient QueueManager::dequeuePQueue() {
-    Patient op;
+    Patient patient;
 
     //check if high is not empty
     //then if med is not empty
     //then if low is not empty
 
     //return null if all are empty
-    if(!high_queue.empty()){
-        op = high_queue.top();
+    if (!high_queue.empty()){
+        patient = high_queue.top();
         high_queue.pop();
     }
-    else if(!medium_queue.empty()){
-        op = medium_queue.top();
+    else if (!medium_queue.empty()){
+        patient = medium_queue.top();
         medium_queue.pop();
     }
-    else if(!low_queue.empty()){
-        op = low_queue.top();
+    else if (!low_queue.empty()){
+        patient = low_queue.top();
         low_queue.pop();
     }
 
-    return op;
+    return patient;
 }
 
 //need to check for node validity?
 void QueueManager::enqueueEventQueue(EventNode event_node) {
     event_queue.push(event_node);
-    
 }
 
 EventNode QueueManager::dequeueEventQueue() {
-
-    EventNode op;
+    EventNode event;
     if(!event_queue.empty()){
-        op = event_queue.top();
+        event = event_queue.top();
         event_queue.pop();
     }
-    return op;
+    return event;
 }
